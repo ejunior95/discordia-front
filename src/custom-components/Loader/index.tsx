352a79c332d6a3fb7e6fcfa7f11719a3c7277a6c
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 21:19:40 -0300

    feat: Alterando loader

--- src/custom-components/Loader/index.tsx
+++ src/custom-components/Loader/index.tsx
@@ -6,7 +6,7 @@ export default function Loader() {
             <div
             className='w-20 h-20 border-6 border-t-background border-foreground rounded-full animate-spin'
             ></div>
-            <p>Carregando...</p>
+            <p className='mt-2'>Carregando...</p>
         </div>
     )
 }
