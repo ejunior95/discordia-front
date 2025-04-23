1a47887a5f3feb815e70026abd2152966f92c6f6
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:15:38 -0300

    feat: Atualizando estrutura do componente de login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,60 +9,118 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { getUserInfo, login } from "@/services/auth.service"
+import { useState } from "react"
+import Loader from "@/custom-components/Loader"
+import { toast } from "sonner"
+import { Eye, EyeClosed } from "lucide-react"
+import Logo from "../assets/discordia-logo-removebg2.png"
+import { useAuth } from "@/hooks/useAuth"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [visiblePassword, setVisiblePassword] = useState<string>('password');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    e.preventDefault();
+  
+    const email = (e.currentTarget as any).email.value;
+    const password = (e.currentTarget as any).password.value;
+  
+    try {
+      await login(email, password);
+      const user = await getUserInfo();
+      setUser(user);
+      navigate("/home");
+    } catch (err) {
+      console.error("Erro ao fazer login", err);
+      toast("Erro ao fazer login", {
+        description: String(err),
+        action: {
+          label: "Detalhes",
+          onClick: () => console.log("Undo"),
+        },
+      })
+    }
+    setLoading(false);
+  };
+
+  const toggleVisiblePassword = () => {
+    visiblePassword === 'password' ?
+    setVisiblePassword('text') :
+    setVisiblePassword('password')
+  }
+
   return (
-    <div className={cn("flex flex-col gap-6 w-100 mt-4", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-lg">Faça o login na sua conta</CardTitle>
-          <CardDescription>
-            Entre com seu email e senha para acessar sua conta
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="email@exemplo.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Senha</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Esqueceu sua senha?
-                  </a>
+    loading ? ( <> <Loader /> </> ) :
+    <>
+      <div className="flex w-100 place-content-center justify-center">
+        <img src={Logo} className="w-15 h-12" alt="logo-discordia" />
+        <span className="text-2xl font-semibold tracking-tighter py-2 pl-2 select-none">DiscordIA</span>
+      </div>
+      <div className={cn("flex flex-col gap-6 w-100 mt-4", className)} {...props}>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-lg">Faça o login na sua conta</CardTitle>
+            <CardDescription>
+              Entre com seu email e senha para acessar sua conta
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="email@exemplo.com"
+                    required
+                  />
                 </div>
-                <Input id="password" type="password" required />
+                <div className="grid gap-3 relative">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Senha</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Esqueceu sua senha?
+                    </a>
+                  </div>
+                  <Input id="password" type={visiblePassword} name="password" className="pr-[2.5rem]" required />
+                  <Button onClick={() => toggleVisiblePassword()} variant="link" size="icon" className="cursor-pointer absolute bottom-0 right-0">
+                    {
+                      visiblePassword === 'password' ? 
+                      ( <> <EyeClosed className="h-[1.2rem] w-[1.2rem]" /> </> ) :
+                      ( <> <Eye className="h-[1.2rem] w-[1.2rem]" /> </> )
+                    }
+                  </Button>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button type="submit" className="w-full cursor-pointer">
+                    Continuar
+                  </Button>
+                </div>
               </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full cursor-pointer">
-                  Continuar
-                </Button>
+              <div className="mt-4 text-center text-sm">
+                Ainda não tem uma conta?{" "}
+                <Link to="/register" className="underline underline-offset-4">
+                  Cadastre-se aqui
+                </Link>
               </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Ainda não tem uma conta?{" "}
-              <Link to="/register" className="underline underline-offset-4">
-                Cadastre-se aqui
-              </Link>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
