6d0c06ae635c3066d592e5382ee7666707fc29c4
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:14:24 -0300

    feat: Protegendo rotas publicas e privadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from './components/theme-provider';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from './custom-components/Loader';
+import ProtectedRoute from './custom-components/ProtectedRoute';
+import { Toaster } from './components/ui/sonner';
+import PublicRoute from './custom-components/PublicRoute';
 
 const Home = lazy(() => import('./pages/Home'));
 const Chat = lazy(() => import('./pages/Chat'));
@@ -14,16 +17,38 @@ export default function App() {
       <BrowserRouter>
         <Suspense fallback={<Loader />}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/" element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } />
+            <Route path="/home" element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } />
+            <Route path="/chat" element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            } />
+            <Route path="/login" element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            } />
+            <Route path="/register" element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            } />
             {/* Nested route with layout
             <Route path="/dashboard" element={<DashboardLayout />}>
               <Route path="stats" element={<Stats />} />
             </Route> */}
           </Routes>
         </Suspense>
+        <Toaster />
       </BrowserRouter>
     </ThemeProvider>
   );
