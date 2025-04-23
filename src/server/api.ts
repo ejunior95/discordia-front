e1390e0523c2db5b712e3c97fdff4ab97dc903dd
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:20:38 -0300

    feat: Passando credentials na agencia do arquivo api

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL_LOCAL;
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 if (!baseURL) {
-  throw new Error('A variável VITE_API_BASE_URL_LOCAL não está definida!');
+  throw new Error('A variável VITE_API_BASE_URL não está definida!');
 }
 
-export const api = axios.create({ baseURL });
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+});
