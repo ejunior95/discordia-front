45e81e9f7a73d9c3e04e4609b42b476faf16902d
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:18:49 -0300

    feat: Validação de user no carregamento da pagina Chat

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,27 @@
 import { ChatBody } from "@/custom-components/ChatBody"
 import { Navbar } from "@/custom-components/Navbar"
+import { useAuth } from "@/hooks/useAuth";
+import { getUserInfo } from "@/services/auth.service";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Chat () {
+    const { setUser } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+          const user = await getUserInfo();
+          if (user) {
+            setUser(user);
+          } else {
+            navigate('/login');
+          }
+        };
+        fetchUser();
+      }, []);
+      
+
     return(
         <>
             <Navbar />
