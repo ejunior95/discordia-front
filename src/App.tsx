eafc72f61c1f6a4ea5316d5b38aa520458fd6eb6
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-24 23:56:04 -0300

    feat: Implementando o Layout padrão de tela na raiz do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Loader from './custom-components/Loader';
 import ProtectedRoute from './custom-components/ProtectedRoute';
 import { Toaster } from './components/ui/sonner';
 import PublicRoute from './custom-components/PublicRoute';
+import { Layout } from './custom-components/Layout';
 
 const Home = lazy(() => import('./pages/Home'));
 const Chat = lazy(() => import('./pages/Chat'));
@@ -17,35 +18,33 @@ export default function App() {
       <BrowserRouter>
         <Suspense fallback={<Loader />}>
           <Routes>
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            } />
-            <Route path="/home" element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            } />
-            <Route path="/chat" element={
-              <ProtectedRoute>
-                <Chat />
-              </ProtectedRoute>
-            } />
-            <Route path="/login" element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            } />
-            <Route path="/register" element={
-              <PublicRoute>
-                <Register />
-              </PublicRoute>
-            } />
-            {/* Nested route with layout
-            <Route path="/dashboard" element={<DashboardLayout />}>
-              <Route path="stats" element={<Stats />} />
-            </Route> */}
+          <Route element={<Layout />}>
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              } />
+              <Route path="/home" element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              } />
+              <Route path="/chat" element={
+                <ProtectedRoute>
+                  <Chat />
+                </ProtectedRoute>
+              } />
+              <Route path="/login" element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              } />
+              <Route path="/register" element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              } />
+            </Route>
           </Routes>
         </Suspense>
         <Toaster />
