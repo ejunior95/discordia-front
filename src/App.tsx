3490b96305b34bbf7c79ad62cdc68da157653839
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-26 02:46:42 -0300

    feat: Criando página LandingPage de apresentação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from './components/theme-provider';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import { Toaster } from './components/ui/sonner';
+import { Layout } from './custom-components/Layout';
 import Loader from './custom-components/Loader';
 import ProtectedRoute from './custom-components/ProtectedRoute';
-import { Toaster } from './components/ui/sonner';
 import PublicRoute from './custom-components/PublicRoute';
-import { Layout } from './custom-components/Layout';
 
 const Home = lazy(() => import('./pages/Home'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
 const Chat = lazy(() => import('./pages/Chat'));
 const Login = lazy(() => import('./pages/Login'));
 const Register = lazy(() => import('./pages/Register'));
@@ -18,24 +19,34 @@ export default function App() {
       <BrowserRouter>
         <Suspense fallback={<Loader />}>
           <Routes>
-          <Route element={<Layout />}>
-              <Route path="/" element={<Home />}/>
-              <Route path="/home" element={<Home />}/>
+            <Route element={<Layout />}>
+              
+              <Route path="/" element={<LandingPage />}/>
+
+              <Route path="/home" element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              } />
+
               <Route path="/chat" element={
                 <ProtectedRoute>
                   <Chat />
                 </ProtectedRoute>
               } />
+
               <Route path="/login" element={
                 <PublicRoute>
                   <Login />
                 </PublicRoute>
               } />
+
               <Route path="/register" element={
                 <PublicRoute>
                   <Register />
                 </PublicRoute>
               } />
+
             </Route>
           </Routes>
         </Suspense>
