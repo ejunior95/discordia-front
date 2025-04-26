45fa288a792e407d171801e4ec47a5744ff6bf08
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-25 21:01:09 -0300

    feat: Removendo navbar nas rotas de home

--- src/custom-components/Layout.tsx
+++ src/custom-components/Layout.tsx
@@ -4,7 +4,7 @@ import { Outlet, useLocation } from "react-router-dom"
 export const Layout = () => {
   const location = useLocation()
 
-  const hideNavbarOn = ['/login', '/register']
+  const hideNavbarOn = ['/login', '/register', '/home', '/']
 
   const hideNavbar = hideNavbarOn.includes(location.pathname)
 
