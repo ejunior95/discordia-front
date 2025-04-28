26d3484fb09566305665a31d8cda54c70d3cb4be
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 02:41:23 -0300

    feat: Implementando Loader para aguardar até o user for setado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -12,24 +12,28 @@ export type CurrentUser = {
 type AuthContextType = {
   user: CurrentUser | null;
   setUser: (user: CurrentUser | null) => void;
+  isLoading: boolean;
 };
 
 export const AuthContext = createContext<AuthContextType>({
   user: null,
   setUser: () => {},
+  isLoading: true,
 });
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<CurrentUser | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getUserInfo().then((data) => {
       if (data) setUser(data);
+      setIsLoading(false);
     });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
