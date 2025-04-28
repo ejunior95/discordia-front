150e6de6e845c89eb6f7239c5682ef59467ea129
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 02:42:35 -0300

    feat: Criando nova rota de jogos

---
+++ src/pages/Chess.tsx
@@ -0,0 +1,7 @@
+export default function Chess() {
+    return(
+        <section className="h-[100dvh] flex flex-col justify-center items-center">
+            <p>XADRZEW</p>
+        </section>
+    )
+}
