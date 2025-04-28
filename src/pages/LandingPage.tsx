1fc8debc4958cb64365a4d1123e2b0a9ddbec0b1
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 03:04:22 -0300

    feat: Aumentando margem no titulo da section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -136,7 +136,9 @@ export default function LandingPage() {
         </motion.div>
         
         <div className=" z-10 flex flex-col items-center max-w-4xl h-full w-full">
-          <h2 className="scroll-m-20 mt-[1vh] text-4xl xl:text-4xl font-extrabold tracking-tight lg:text-5xl text-white w-full text-center">O que você pode fazer com o DiscordIA?</h2>
+          <h2 className="text-4xl xl:text-4xl font-extrabold tracking-tight lg:text-5xl text-white w-full text-center mt-8">
+            O que você pode fazer com o DiscordIA?
+          </h2>
           <div className="relative w-full overflow-hidden">
             <div className="flex transition-transform duration-700"
               style={{ transform: `translateX(-${active * 100}%)` }}
@@ -182,8 +184,8 @@ export default function LandingPage() {
         </div>
       </section>
 
-      <section className="h-[100dvh] xl:h-[120dvh] 2xl:h-[100dvh] relative flex flex-col justify-center items-center bg-gradient-to-bl from-black to-gray-900  bg-gray-50 px-6 text-center">
-        <h2 className="text-4xl font-semibold mb-8">Assine para aproveitar todos os serviços</h2>
+      <section className="flex flex-col lg:h-dvh justify-center items-center bg-gradient-to-bl from-black to-gray-900  bg-gray-50 px-6 text-center">
+        <h2 className="text-4xl font-semibold mt-8 mb-12 lg:mt-8 ">Assine para aproveitar todos os serviços</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-8 max-w-6xl w-full">
           {prices.map((plan) => (
             <motion.div
