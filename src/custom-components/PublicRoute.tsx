ca33d7200a29fdd2e21053a4ab2fb4ca970069a2
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 02:46:13 -0300

    feat: Implementando Loader para aguardar até o user for setado

--- src/custom-components/PublicRoute.tsx
+++ src/custom-components/PublicRoute.tsx
@@ -1,8 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
+import Loader from "./Loader";
 
 export default function PublicRoute({ children }: { children: React.ReactNode }) {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   if (user) {
     return <Navigate to="/home" replace />;
