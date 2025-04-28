bfb07a33871a65fbdc74bde87bcc1fc6ccc1b07b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 02:42:20 -0300

    feat: Implementando Loader para aguardar até o user for setado

--- src/custom-components/ProtectedRoute.tsx
+++ src/custom-components/ProtectedRoute.tsx
@@ -1,11 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
 import { JSX } from "react/jsx-runtime";
+import Loader from "./Loader";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
 
-  if (!user) return <Navigate to="/login" />;
+  if (isLoading) {
+    return <Loader />
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
 
   return children;
 }
