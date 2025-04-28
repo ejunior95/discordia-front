24710fd53d2bbd7acbea6019c4fd8af056a5a867
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-28 00:52:52 -0300

    feat: Iniciando página Games

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,5 +1,15 @@
 export default function Games() {
     return(
-        <div>Jogos</div>
+        <section className="h-[100dvh] flex flex-col justify-center items-center">
+            <h1 className="scroll-m-20 text-8xl font-extrabold tracking-tight ">
+                Jogos com IA
+            </h1>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Once upon a time, in a far-off land, there was a very lazy king who
+              spent all day lounging on his throne. One day, his advisors came to him
+              with a problem: the kingdom was running out of money.
+            </p>
+        </section>
+        
     )
 }
