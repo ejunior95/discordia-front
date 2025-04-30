6b9473c0bf3bf2f9d45331de363184bd8ea50989
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-30 11:59:20 -0300

    feat: Criando estrutura da página home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
 export default function Home() {
     return(
-        <div>
-            <p>Home</p>
-        </div>
+        <section className="
+            p-10
+            w-full 
+            flex 
+            flex-col 
+            items-center
+            2xl:h-[90dvh] 
+        ">
+            <h1 className="
+                font-extrabold 
+                tracking-tight 
+                text-5xl 
+                mb-5 
+                md:text-6xl 
+                md:mb-8 
+                xl:text-7xl 
+                xl:mb-8 
+                2xl:mb-10
+                w-full 
+                lg:w-[80%]
+                2xl:w-[60%] 
+                2xl:max-w-[1200px]">
+                Página inicial
+            </h1>
+        </section>
     )
 }
