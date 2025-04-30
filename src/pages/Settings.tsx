a84901cb19edb9922bc4ba53b371c2d4925dbb8a
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-30 12:40:40 -0300

    feat: Alterando nome sharedFunctions para globalFunctions

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useAuth } from "@/hooks/useAuth";
-import { formatFallbackAvatarStr } from "@/utils/sharedFunctions";
+import { formatFallbackAvatarStr } from "@/utils/globalFunctions";
 
 export default function Settings() {
     const { user } = useAuth();
