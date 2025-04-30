c8bed5b7fa3102e144c88894e6a1571f14a5fc3c
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-30 12:00:08 -0300

    feat: Criando página NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { ArrowLeft } from 'lucide-react';
+import { useAuth } from '@/hooks/useAuth';
+
+export default function NotFound() {
+    const { user } = useAuth();
+
+    return (
+        <div className="flex h-[90dvh] lg:h-[80dvh] items-center justify-center px-6">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: 'easeOut' }}
+            className="text-center max-w-md border p-8 rounded-2xl shadow-xl"
+          >
+            <motion.h1
+              className="text-7xl font-extrabold text-red-600"
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              404
+            </motion.h1>
+            <p className="mt-4 text-xl font-semibold">
+              Página não encontrada
+            </p>
+            <p className="mt-2">
+              Opa! A rota que você tentou acessar não existe ou foi movida.
+            </p>
+
+            <Link
+              to={ user ? "/home" : "/" }
+              className="flex w-full justify-center items-center gap-2 mt-6 px-5 py-3 rounded-md text-white bg-indigo-600 hover:bg-indigo-700 transition-all duration-300"
+            >
+              <ArrowLeft size={18} />
+              Voltar para o início
+            </Link>
+          </motion.div>
+        </div>
+    );
+}
