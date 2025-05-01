15e430c47f8831d705096accbb79c22da2d24f98
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-01 00:29:59 -0300

    feat: Criando método de update

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,23 @@
 import { api } from "@/server/api";
 
 export default class UserService {
-    async create(data: FormData) {
+
+  async create(data: FormData) {
         const response = await api.request({
           method: 'POST',
           url: 'users',
+          data,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        return response;
+    }
+
+    async update(id: string, data: FormData) {
+        const response = await api.request({
+          method: 'PATCH',
+          url: `users/${id}`,
           data,
           headers: {
             'Content-Type': 'multipart/form-data',
