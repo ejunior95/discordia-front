c93168661f3f93f2cb759c62a00277ac594fedbb
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-08 03:19:32 -0300

    feat: Implementando componente que sinaliza atua IA

--- src/custom-components/TagCurrentIA.tsx
+++ src/custom-components/TagCurrentIA.tsx
@@ -1,7 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { CurrentUser } from "@/contexts/AuthContext"
+import { formatFallbackAvatarStr } from "@/utils/globalFunctions"
 import { DeepSeek, Gemini, Grok, OpenAI } from "@lobehub/icons"
+import IconVersus from "../assets/icon-versus.svg"
 
 export interface ITagCurrentIA {
     ia: 'gemini' | 'grok' | 'chat-gpt' | 'deepseek'
+    user: CurrentUser
 }
 
 export function TagCurrentIA(props: ITagCurrentIA) {
@@ -10,35 +15,49 @@ export function TagCurrentIA(props: ITagCurrentIA) {
             title: 'Gemini',
             sub: 'gemini-2.0-flash',
             icon: <Gemini size={40} />,
-            style: 'bg-white text-blue-600 border '
         },
         grok: {
             title: 'Grok',
             sub: 'grok-3-beta',
             icon: <Grok size={40} />,
-            style: 'bg-gray-600 text-white '
         },
         deepseek: {
             title: 'Deepseek',
             sub: 'deepseek-reasoner',
             icon: <DeepSeek size={50} />,
-            style: 'bg-blue-600 text-white '
         },
         'chat-gpt': {
             title: 'Chat GPT',
             sub: 'gpt-4o',
             icon: <OpenAI size={40} />,
-            style: 'bg-black text-white-600 border-white'
         },
     }
     return(
-        <div className={`${objIA[props.ia].style} md:space-x-2 md:p-4 md:w-[8dvw] md:justify-between flex justify-center items-center p-4 rounded-full`}>
-            { objIA[props.ia].icon }
-            <div className="hidden md:block md:space-y-1 md:w-[4dvw]">
-                <p className="text-[12px] py-1 bg-red-600 w-[65%] flex justify-center rounded tracking-wide">CONTRA</p>
-                <p className="text-lg font-medium leading-none">{objIA[props.ia].title}</p>
-                <p className={`text-sm text-gray-300`}>{ objIA[props.ia].sub }</p>
+        <div className="absolute w-full left-0 top-20 flex justify-center items-center">
+            <div className="px-6 bg-blue-500 flex justify-between items-center rounded-b-lg">
+                
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-12 w-12 rounded-full">
+                    <AvatarImage src={props.user?.avatar} alt={props.user?.name} className="object-cover object-center" />
+                    <AvatarFallback>{formatFallbackAvatarStr(props.user!)}</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">{props.user?.name}</span>
+                    <span className="truncate text-xs">{props.user?.email}</span>
+                  </div>
+                </div>
+
+                <img src={IconVersus} className="h-12 w-12" alt="VS" />
+
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold text-right">{objIA[props.ia].title}</span>
+                    <span className="truncate text-xs text-right">{ objIA[props.ia].sub }</span>
+                  </div>
+                    { objIA[props.ia].icon }
+                </div>
+
             </div>
-        </div>
+          </div>
     )
 }
