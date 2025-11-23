3502f0df3450e860f2b5fbbcae1dda8b723717fd
Edvaldo de Ramos Junior <edvaldo.ramos@castgroup.com.br>
2025-11-22 22:32:43 -0300

    chore: Refactor EasyChat integration to conditionally render based on environment flag and update theme to dark.

--- src/main.tsx
+++ src/main.tsx
@@ -6,23 +6,26 @@ import { AuthProvider } from './contexts/AuthContext.tsx'
 import { EasyChat } from '@ejunior95/easy-chat';
 import '@ejunior95/easy-chat/dist/style.css';
 
+const flagPageIsCreating = import.meta.env.VITE_FLAG_ISWORKING;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <App />
-      <EasyChat
-          config={{
-            title: "DiscordIA Chatbot",
-            position: "bottom-right",
-            primaryColor: "#007bff",
-            systemPrompt: `Você é um assistente virtual chamado Disc, especializado em fornecer informações e suporte sobre a plataforma DiscordIA. 
-            Seja educado, prestativo e forneça respostas claras e concisas às perguntas dos usuários relacionadas ao DiscordIA.
-            Basicamente, para te dar mais contexto, o DiscordIA é uma plataforma onde os usuários colocam os agentes de IA Gemini, Grok, ChatGPT e Deepseek para disputar entre si em batalhas de IA, 
-            onde o objetivo é entreter os espectadores e proporcionar uma experiência divertida e interativa.`,
-            theme: "light",
-            initialMessage: `Olá! Eu sou o Disc. Como posso ajudar você hoje`,
-          }} 
-        />
+      <>
+        {flagPageIsCreating !== "true" && (
+          <EasyChat
+            config={{
+              position: "bottom-right",
+              primaryColor: "#007bff",
+              systemPrompt: `Você é um assistente virtual chamado Disc, especializado em fornecer informações e suporte sobre a plataforma DiscordIA. 
+                  Seja educado, prestativo e forneça respostas claras e concisas às perguntas dos usuários relacionadas ao DiscordIA.
+                  Basicamente, para te dar mais contexto, o DiscordIA é uma plataforma onde os usuários colocam os agentes de IA Gemini, Grok, ChatGPT e Deepseek para disputar entre si em batalhas de IA, 
+                  onde o objetivo é entreter os espectadores e proporcionar uma experiência divertida e interativa.`,
+              theme: "dark",
+            }}
+          />
+        )}
+      </>
     </AuthProvider>
   </StrictMode>,
 )
